b813d00ca53c6424905aca125c3d855192574665
huongpx <huong.px01@gmail.com>
2023-07-11 11:12:45 +0700

    add header with Nuxt logo and nav-links, login, sign up buttons

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheHeader from '../components/TheHeader.vue';
+
+
+</script>
+
+
+<template>
+  <div>
+    <TheHeader />
+    <div class="max-w-[1280px] mx-auto my-5">
+      <slot />
+    </div>
+  </div>
+</template>
+
