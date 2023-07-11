a8a376163e22104c348eb9c5651ab12278e67a7e
huongpx <huong.px01@gmail.com>
2023-07-11 09:41:23 +0700

    init project: primevue, vuefire

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  devtools: { enabled: true },
+  modules: ['nuxt-vuefire'],
+
+  vuefire: {
+    auth: true,
+    config: {
+      apiKey: "AIzaSyCeOXaWw4JseqBjboyK91fzNP1RKU5Xus4",
+      authDomain: "sample-django-ae14d.firebaseapp.com",
+      projectId: "sample-django-ae14d",
+      storageBucket: "sample-django-ae14d.appspot.com",
+      messagingSenderId: "929995888235",
+      appId: "1:929995888235:web:3425d2ac89a33bb9ba832e",
+      measurementId: "G-3SNL86G6LH"
+    }
+  },
+
+  css: [
+    "primevue/resources/themes/mdc-light-indigo/theme.css"
+  ],
+
+  build: {
+    transpile: ["primevue"]
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  // middleware: 'auth',
+})
+
+</script>
+
+
+<template>
+  <div>
+    Index
+  </div>
+</template>
+
