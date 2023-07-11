2e1c8f37708dd278d785bd118c4a780d81cca718
huongpx <huong.px01@gmail.com>
2023-07-11 23:41:58 +0700

    WIP: enhance footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,13 +6,48 @@
 <template>
   <section class="">
     <Divider />
-    <div class="m-container flex">
-      <div id="footer-contact">
-        <div class="text-4xl">
-          <img src="/nuxt-icon.svg" style="height: 2.6rem;">
-          NUXTCARS
+    <div class="m-container flex flex-col lg:flex-row">
+      <div id="footer-contact" class="mb-5 columns-1 md:columns-3 lg:columns-1 lg:w-1/4">
+        <div id="logo"><img src="/nuxt-icon.svg" style="height: 2.6rem;"></div>
+        <div id="phone-email">
+          <div>Phone</div>
+          <div>email</div>
         </div>
+        <div id="social">social</div>
+      </div>
+      <div id="footer-nav" class="text-sm columns-1 md:columns-4 lg:w-3/4">
+        <div>Chinh sach va quy dinh</div>
+        <div>col1</div>
+        <div>col1</div>
+        <div>col1</div>
       </div>
     </div>
   </section>
 </template>
+
+
+<style scoped lang="scss">
+#footer-contact {
+  border: 1px solid blue;
+
+  #logo {
+    border: 1px solid blueviolet;
+  }
+
+  #phone-email {
+    border: 1px solid blueviolet;
+  }
+
+  #social {
+    border: 1px solid blueviolet;
+  }
+}
+
+#footer-nav {
+  border: 1px solid yellow;
+
+  &:nth-child(1) {
+    border: 1px solid blueviolet;
+  }
+}
+</style>
